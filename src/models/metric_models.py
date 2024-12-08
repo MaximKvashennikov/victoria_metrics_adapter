5ee1942fc057b0a04f39736c84106dd63a25e709
Maxim Kvashennikov <kvashennickov.maxim@yandex.ru>
2024-12-08 18:46:59 +0300

    Changed models inheritance

--- src/models/metric_models.py
+++ src/models/metric_models.py
@@ -2,7 +2,7 @@ from pydantic import BaseModel, field_validator, Field, ConfigDict
 from typing import Optional, Union, List
 
 
-class MetricLabel(BaseModel):
+class BaseMetricLabel(BaseModel):
     model_config = ConfigDict(populate_by_name=True)
 
     metric_name: str = Field(alias="__name__")
@@ -10,13 +10,21 @@ class MetricLabel(BaseModel):
     instance: str = "6c65dfec-d15c-45a2-83ef-a7b42fb32527"
     application_id: str = "BaseApp-a9506d78"
     application_instance_id: str = "Modelling"
-    security: str = "Unsafe"
     service_instance_id: str = "6c65dfec-d15c-45a2-83ef-a7b42fb32527"
     service_name: str = "Tme"
     service_version: str = "1.0.0"
     telemetry_sdk_language: str = "dotnet"
     telemetry_sdk_name: str = "opentelemetry"
     telemetry_sdk_version: str = "1.6.0"
+
+class BaseMetricData(BaseModel):
+    metric: BaseMetricLabel
+    values: List[Union[int, float]]
+    timestamps: List[int]
+
+
+class MetricLabel(BaseMetricLabel):
+    security: str = "Unsafe"
     step_count: Optional[str] = None
     risk_name: Optional[str] = None
 
@@ -28,7 +36,6 @@ class MetricLabel(BaseModel):
         return v
 
 
-class MetricData(BaseModel):
+class MetricData(BaseMetricData):
     metric: MetricLabel
-    values: List[Union[int, float]]
-    timestamps: List[int]
+
